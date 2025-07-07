1356c43bc72f5cfe497943b3cff51e3709e8dc0e
Roman Ivanov <sitdoff@gmail.com>
2025-07-07 19:52:05 +0300

    rename base model

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from src.config import settings
-from src.utils import AppBaseModel
+from src.models import *
+from src.utils import BaseModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = AppBaseModel.metadata
+target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/utils/base_model.py
+++ src/utils/base_model.py
@@ -1,11 +1,9 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 
-class AppBaseModel(DeclarativeBase):
+class BaseModel(DeclarativeBase):
     __abstract__ = True
     metadata = MetaData(naming_convention=settings.db.convention)
-
-    id: Mapped[int] = mapped_column(primary_key=True)
