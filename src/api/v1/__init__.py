2e76f8049024492274668a337a4036d4e83a82d8
Roman Ivanov <sitdoff@gmail.com>
2025-07-07 19:49:59 +0300

    remove example app

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 
-from src.api.v1.example_app import router as example_router
 from src.config import settings
 
 router = APIRouter(prefix=settings.api.v1_prefix)
-router.include_router(example_router)
