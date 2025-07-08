3670f74b60f422461301c5e1bfea76bd9944b27f
Roman Ivanov <sitdoff@gmail.com>
2025-07-08 18:17:33 +0300

    update wallet schema and balance schema

--- src/schemas/wallet/wallet.py
+++ src/schemas/wallet/wallet.py
@@ -4,9 +4,17 @@ from pydantic import BaseModel
 
 
 class WalletSchema(BaseModel):
+    """
+    Схема кошелька.
+    """
+
     uuid: str
     balance: Decimal
 
 
 class WalletBalance(BaseModel):
+    """
+    Схема баланса кошелька.
+    """
+
     balance: Decimal
