5d4273560a32a34f888a3275e9c6a52d8aba3d74
Roman Ivanov <sitdoff@gmail.com>
2025-07-08 18:33:49 +0300

    update wallet use case

--- src/usecases/wallets/usecases.py
+++ src/usecases/wallets/usecases.py
@@ -15,16 +15,25 @@ class WalletUseCase(BaseUseCase):
         self.service = service
 
     async def get_all_wallets(self) -> list[WalletSchema]:
+        """
+        Получение данных о всех кошельках из базы данных.
+        """
         wallets: Sequence[WalletModel] = await self.service.get_all_wallets()
         return [
             WalletSchema(uuid=wallet.uuid, balance=wallet.balance) for wallet in wallets
         ]
 
     async def get_wallet_balance(self, uuid: UUID) -> WalletBalance:
+        """
+        Получение данных о кошельке
+        """
         wallet: WalletModel = await self.service.get_wallet(uuid)
         return WalletBalance(balance=wallet.balance)
 
     async def create_wallet(self) -> WalletSchema:
+        """
+        Создание кошелька
+        """
         wallet: WalletModel = await self.service.create_wallet()
         return WalletSchema(uuid=wallet.uuid, balance=wallet.balance)
 
@@ -33,6 +42,9 @@ class WalletUseCase(BaseUseCase):
         uuid: UUID,
         operation_scheme: OperationSchema,
     ) -> WalletBalance:
+        """
+        Изменение баланса кошелька
+        """
         async with self.session.begin():
             if operation_scheme.operation_type == "DEPOSIT":
                 await self.service.increase_balance(uuid, operation_scheme.amount)
