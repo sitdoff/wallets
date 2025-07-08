d29932576c4430bfa45fd82e1e6b724522100521
Roman Ivanov <sitdoff@gmail.com>
2025-07-08 18:17:07 +0300

    update operation schema

--- src/schemas/wallet/operation.py
+++ src/schemas/wallet/operation.py
@@ -1,9 +1,13 @@
 from decimal import Decimal
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class OperationSchema(BaseModel):
+    """
+    Схема операции с кошельком.
+    """
+
     operation_type: Literal["DEPOSIT", "WITHDRAW"]
-    amount: Decimal
+    amount: Decimal = Field(gt=0)
