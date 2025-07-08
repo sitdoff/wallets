a9347b2a271ba8abecf3b44678e24d4fe1efb7c4
Roman Ivanov <sitdoff@gmail.com>
2025-07-08 18:33:40 +0300

    update wallet service

--- src/services/wallets/services.py
+++ src/services/wallets/services.py
@@ -10,24 +10,43 @@ from src.utils.base_service import BaseService
 
 
 class WalletService(BaseService):
+    """
+    Сервис с логикой для кошельков
+    """
+
     def __init__(self, session: AsyncSession):
         self.repository = WalletRepository(session)
 
     async def get_all_wallets(self) -> Sequence[WalletModel]:
+        """
+        Получение списка всех кошельков
+        """
         return await self.repository.get_all()
 
     async def get_wallet(self, uuid: UUID) -> WalletModel:
+        """
+        Получение кошелька по uuid
+        """
         return await self.repository.get_by_uuid(uuid)
 
     async def create_wallet(self) -> WalletModel:
+        """
+        Создание данных
+        """
         return await self.repository.create()
 
     async def increase_balance(self, uuid: UUID, amount: Decimal) -> WalletModel:
+        """
+        Увеличение баланса кошелька
+        """
         wallet: WalletModel = await self.repository.get_by_uuid(uuid, for_update=True)
         wallet.balance += amount
         return wallet
 
     async def decrease_balance(self, uuid: UUID, amount: Decimal) -> WalletModel:
+        """
+        Уменьшение баланса кошека
+        """
         wallet: WalletModel = await self.repository.get_by_uuid(uuid, for_update=True)
         if wallet.balance < amount:
             raise ValueError("Insufficient funds")
