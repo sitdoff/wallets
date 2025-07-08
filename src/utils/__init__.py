4891013595c6aea495d0640fbc240ef54b47a2cd
Roman Ivanov <sitdoff@gmail.com>
2025-07-08 19:55:56 +0300

    clear utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,1 @@
-from src.utils.base_model import BaseModel
-from src.utils.base_repository import BaseRepository
-from src.utils.base_service import BaseService
-from src.utils.base_usecase import BaseUseCase
 from src.utils.mixins import IdMixin
