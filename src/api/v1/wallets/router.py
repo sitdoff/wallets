ff64742a574f1d866668b2aa29105b21d2a28c28
Roman Ivanov <sitdoff@gmail.com>
2025-07-09 22:29:22 +0300

    rework routes

--- src/api/v1/wallets/router.py
+++ src/api/v1/wallets/router.py
@@ -1,40 +1,40 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Body, Depends, Path
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.v1.wallets.docs import docs as wallets_docs
 from src.database import db_helper
 from src.schemas import OperationSchema, WalletBalance, WalletSchema
-from src.services import WalletService
-from src.usecases import WalletUseCase
+from src.usecases import WalletUseCase, get_usecase
 
 router = APIRouter()
 
 
 @router.get("/all", **wallets_docs["all"])
 async def get_all_wallets(
-    session: Annotated[AsyncSession, Depends(db_helper.get_session)],
+    usecase: Annotated[WalletUseCase, Depends(get_usecase)],
 ) -> list[WalletSchema]:
-    usecase = WalletUseCase(session=session, service=WalletService(session))
     return await usecase.get_all_wallets()
 
 
 @router.get("/{uuid}", **wallets_docs["balance"])
 async def get_balance(
     uuid: Annotated[UUID, Path(description="Уникальный идентификатор кошелька")],
-    session: Annotated[AsyncSession, Depends(db_helper.get_session)],
+    usecase: Annotated[WalletUseCase, Depends(get_usecase)],
 ) -> WalletBalance:
-    usecase = WalletUseCase(session=session, service=WalletService(session))
-    return await usecase.get_wallet_balance(uuid)
+    try:
+        return await usecase.get_wallet_balance(uuid)
+    except NoResultFound as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc))
 
 
 @router.post("/create", **wallets_docs["create"])
 async def create_wallet(
-    session: Annotated[AsyncSession, Depends(db_helper.get_session)],
+    usecase: Annotated[WalletUseCase, Depends(get_usecase)],
 ) -> WalletSchema:
-    usecase = WalletUseCase(session=session, service=WalletService(session))
     return await usecase.create_wallet()
 
 
@@ -42,8 +42,11 @@ async def create_wallet(
 async def change_balance(
     uuid: Annotated[UUID, Path(description="Уникальный идентификатор кошелька")],
     operation_scheme: Annotated[OperationSchema, Body()],
+    usecase: Annotated[WalletUseCase, Depends(get_usecase)],
     session: Annotated[AsyncSession, Depends(db_helper.get_session)],
 ) -> WalletBalance:
-    usecase = WalletUseCase(session=session, service=WalletService(session))
     async with session.begin():
-        return await usecase.change_wallet_balance(uuid, operation_scheme)
+        try:
+            return await usecase.change_wallet_balance(uuid, operation_scheme)
+        except NoResultFound as exc:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc))
