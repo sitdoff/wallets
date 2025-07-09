0b31478ad036528acdc5ecea75ae28344b3059ba
Roman Ivanov <sitdoff@gmail.com>
2025-07-09 22:50:35 +0300

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,64 @@
+from unittest.mock import AsyncMock, MagicMock, create_autospec
+
+import pytest
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
+                                    async_sessionmaker, create_async_engine)
+from testcontainers.postgres import PostgresContainer
+
+from src.models import BaseModel
+from src.repositories import WalletRepository
+from src.services import WalletService
+from src.usecases import WalletUseCase
+
+
+@pytest.fixture(scope="session")
+def postgres_container():
+    print("START CONTAINER")
+    with PostgresContainer("postgres:16", driver="asyncpg") as container:
+        yield container
+
+
+@pytest.fixture(scope="session")
+async def db_engine(postgres_container: PostgresContainer):
+    engine: AsyncEngine = create_async_engine(
+        url=postgres_container.get_connection_url(),
+        poolclass=NullPool,
+    )
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
+        await conn.run_sync(BaseModel.metadata.create_all)
+    yield engine
+
+
+@pytest.fixture
+async def session(db_engine):
+    session_factory = async_sessionmaker(
+        bind=db_engine,
+        autoflush=False,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
+    async with session_factory() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+
+@pytest.fixture
+async def repository(session):
+    repo = WalletRepository(session)
+    return repo
+
+
+@pytest.fixture
+async def service(session):
+    _service = WalletService(session)
+    return _service
+
+
+@pytest.fixture
+async def wallet_usecase(session: AsyncSession):
+    service = WalletService(session)
+    return WalletUseCase(session, service)
